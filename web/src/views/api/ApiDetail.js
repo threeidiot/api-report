6428ccb518441e33ce572f3a13bd910a0629d0d9
wanghuida <wanghuida258@aliyun.com>
2018-02-11 17:24:08 +0800

    全局参数

--- web/src/views/api/ApiDetail.js
+++ web/src/views/api/ApiDetail.js
@@ -4,6 +4,7 @@ import { Tag, Button, Divider, Row, Col, Form, Input, Icon } from 'antd'
 import * as api from 'helpers/api'
 import ReactJson from 'react-json-view'
 import ParamEditModal from 'views/param/ParamEditModal'
+import DefParamEditModal from 'views/param/DefParamEditModal'
 import './ApiDetail.scss'
 
 const FormItem = Form.Item
@@ -13,16 +14,25 @@ const FormItem = Form.Item
 class ApiDetail extends Component {
   constructor (props) {
     super(props)
+    // 设置需要使用的 store
     this.projectsStore = props.stores.projectsStore
     this.apisStore = props.stores.apisStore
     this.paramsStore = props.stores.paramsStore
+    this.defParamsStore = props.stores.defParamsStore
 
+    // 初始化数据
     this.projectsStore.setCurrId(props.match.params.pid)
     this.apisStore.setCurrId(props.match.params.aid)
+    this.formItemLayout = {
+      labelCol: { span: 7 },
+      wrapperCol: { span: 17 }
+    }
 
     this.state = { json: '' }
 
+    // 获取数据
     this.paramsStore.fetchRows(this.apisStore.currId)
+    this.defParamsStore.fetchRows(this.projectsStore.currId)
   }
 
   componentWillReceiveProps (props) { }
@@ -33,16 +43,16 @@ class ApiDetail extends Component {
     this.paramEditModal.show(id, this.apisStore.currId)
   }
 
+  openDefParamEditModal (id) {
+    this.paramEditModal.show(id, this.projectsStore.currId)
+  }
+
   getFormItems () {
     const { getFieldDecorator } = this.props.form
-    const formItemLayout = {
-      labelCol: { span: 7 },
-      wrapperCol: { span: 17 }
-    }
 
     const formItems = this.paramsStore.sortedArrRows.map((p, i) => {
       return (
-        <FormItem {...formItemLayout} required={p.required} key={i} help={p.description} label={p.name} >
+        <FormItem {...this.formItemLayout} required={p.required} key={`p${i}`} help={p.description} label={p.name} >
           {
             getFieldDecorator(p.name, {
               initialValue: p.default || '',
@@ -50,12 +60,39 @@ class ApiDetail extends Component {
             })(<Input className='param-input' />)
           }
           <span className='param-type'>
-            <Tag color='purple'>{p.type}</Tag>
             <Tag color='geekblue' onClick={_ => this.openParamEditModal(p.id)}><Icon type='edit' /></Tag>
+            <Tag color='purple'>{p.type}</Tag>
           </span>
         </FormItem>
       )
     })
+
+    return formItems
+  }
+
+  getDefFormItems () {
+    const { getFieldDecorator } = this.props.form
+
+    const formItems = this.defParamsStore.sortedArrRows.filter(p => p.in !== 'header').map((p, i) => {
+      return (
+        <FormItem {...this.formItemLayout} required={p.required} key={`g${i}`} help={p.description} label={p.name} >
+          {
+            getFieldDecorator(p.name, {
+              initialValue: p.default || '',
+              rules: [{ required: p.required, message: '请输入必填字段' }]
+            })(<Input className='param-input' />)
+          }
+          <span className='param-type'>
+            <Tag color='geekblue' onClick={_ => this.openDefParamEditModal(p.id)}><Icon type='edit' /></Tag>
+            <Tag color='purple'>{p.type}</Tag>
+          </span>
+        </FormItem>
+      )
+    })
+
+    if (formItems.length > 0) {
+      formItems.splice(0, 0, <Divider key='divider'>全局参数</Divider>)
+    }
 
     return formItems
   }
@@ -96,6 +133,7 @@ class ApiDetail extends Component {
                 <Button size='small' type='primary' ghost icon='plus'
                   onClick={_ => this.openParamEditModal(0)}>添加参数</Button>
               </FormItem>
+              {this.getDefFormItems()}
             </Form>
           </Col>
 
@@ -110,6 +148,7 @@ class ApiDetail extends Component {
         </Row>
 
         <ParamEditModal wrappedComponentRef={m => (this.paramEditModal = m)} stores={this.props.stores} />
+        <DefParamEditModal wrappedComponentRef={m => (this.paramEditModal = m)} stores={this.props.stores} />
       </div>
     )
   }
