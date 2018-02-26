2a6dafb5c64cd8b61cc5399238ab8982b23f24cc
wanghuida <wanghuida258@aliyun.com>
2018-02-26 10:23:23 +0800

    一键部署测试成功

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,15 +2,17 @@
 
 # laravel 的 sqlite 数据库和配置文件
 touch api/database/database.sqlite
-cp .env.example .env
+cp api/.env.example api/.env
 
-# 打镜像
-docker-compose build
+# 安装依赖 
+docker-compose run composer
+docker-compose run node
+
+# 启动容器
+docker-compose up -d nginx
 
 # laravel key 、数据、优化
 docker-compose exec api php artisan key:generate
 docker-compose exec api php artisan migrate --seed
 docker-compose exec api php artisan optimize
 
-# 启动
-docker-compose up -d nginx
